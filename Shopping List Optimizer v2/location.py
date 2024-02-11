68d06340e0e0738c65243ba5e60cb0c7bca03895
Brendan Knowles <me@brendanknowles.com>
2024-02-11 13:23:41 -0500

    first commit - basic scaffolding

---
+++ Shopping List Optimizer v2/location.py
@@ -0,0 +1,5 @@
+class Location:
+    def __init__(self, store_name : str, address : str, store_sections : dict):
+        self.store_name = store_name
+        self.address = address
+        self.store_sections = store_sections
---
+++ Shopping List Optimizer v2/state.py
@@ -0,0 +1,7 @@
+from location import Location
+from items import Items
+
+class State:
+    def __init__(self, location: Location, items: Items):
+        return 
+    
