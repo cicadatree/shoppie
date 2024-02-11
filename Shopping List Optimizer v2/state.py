e5fb83b2bf50292efe8c489eefe21351d887d34b
Brendan Knowles <me@brendanknowles.com>
2024-02-11 13:29:56 -0500

    fixed State constructor scaffolding

--- Shopping List Optimizer v2/state.py
+++ Shopping List Optimizer v2/state.py
@@ -3,5 +3,7 @@ from items import Items
 
 class State:
     def __init__(self, location: Location, items: Items):
+        self.location = location
+        self.items = items
         return 
     
